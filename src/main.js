f71dc4462adf14d78213c4fb8f54f5f21c3f86c7
cschweda <cschweda@gmail.com>
2019-07-26 10:37:07 -0500

    fix: chrome check

--- src/main.js
+++ src/main.js
@@ -3,20 +3,6 @@ const VueBrowserDetect = {
     let ua = window.navigator.userAgent;
     let browserObj = {};
 
-    //   // IE 10
-    // ua = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)";
-
-    //   // IE 11
-    // ua = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
-
-    //   // Edge 12 (Spartan)
-    // ua =
-    //   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0";
-
-    //   // Edge 13
-    // ua =
-    //   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586";
-
     // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
     browserObj.isOpera =
       !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0;
@@ -35,7 +21,7 @@ const VueBrowserDetect = {
     // Edge 20+
     browserObj.isEdge = !browserObj.isIE && !!window.StyleMedia;
     // Chrome 1+
-    browserObj.isChrome = !!window.chrome
+    browserObj.isChrome = /chrome/i.test(navigator.userAgent);
     // Blink engine detection
     browserObj.isBlink =
       (browserObj.isChrome || browserObj.isOpera) && !!window.CSS;
